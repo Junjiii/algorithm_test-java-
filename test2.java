56cb1a1c6d5bc8109666a1d0e35d828c63584b56
Jjunjiii’ <drum1701@gmail.com>
2024-07-15 14:05:21 +0900

    test2

---
+++ test2.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+
+// 문자 리스트를 문자열로 변환하기
+public class test2 {
+    public String main (String[] arr) {
+        return String.join("", arr);
+    }
+
+    public String solution(String[] arr) {
+        return Arrays.stream(arr).collect(Collectors.joining());
+    }
+}
