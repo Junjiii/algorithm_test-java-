2fd0e7cdfc018816e6820da43c0ee87926a45661
Jjunjiii’ <drum1701@gmail.com>
2024-07-21 10:56:51 +0900

    test11

---
+++ test11_20/test11.java
@@ -0,0 +1,29 @@
+package test11_20;
+
+import java.util.stream.IntStream;
+
+public class test11 {
+
+    public int main(int a, int d, boolean[] included) {
+        return IntStream.range(0, included.length)
+                .filter(i -> included[i])
+                .map(i -> a + d * i)
+                .sum();
+    }
+
+    public int solution1(int a, int d, boolean[] included) {
+        return IntStream.range(0, included.length).map(idx -> included[idx]?a+(idx*d):0).sum();
+    }
+
+    public int solution2(int a, int d, boolean[] included) {
+        int answer = 0;
+
+        for(int i = 0; i < included.length; i++){
+            if(included[i]){
+                answer +=  a + (d*i);
+            }
+        }
+
+        return answer;
+    }
+}
