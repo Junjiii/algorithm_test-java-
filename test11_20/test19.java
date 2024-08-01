e7b42b07a7e786ae8e1a8457adda80c12c44a925
Jjunjiii’ <drum1701@gmail.com>
2024-08-01 13:39:48 +0900

    test19

--- test11_20/test19.java
+++ test11_20/test19.java
@@ -1,23 +1,36 @@
 package test11_20;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class test19 {
-    public int[] solution(int[] arr, int[][] queries) {
-        int[] result = new int[queries.length];
+    public int[] main(int[] arr, int[][] queries) {
 
-        for(int i=0; i<queries.length; i++) {
-            int index = 0;
-            int s = queries[i][0];
-            int e = queries[i][1];
-            int k = queries[i][2];
+        int[] answer = new int[queries.length];
+        Arrays.fill(answer, -1);
 
-            for(int j=s; j<=e; j++) {
-                if(j > k) {
-                    result[index] = j;
-                    break;
+        for (int idx = 0; idx < queries.length; idx++) {
+            int[] query = queries[idx];
+            int s = query[0], e = query[1], k = query[2];
+
+            for (int i = s; i <= e; i++) {
+                if (k < arr[i]) {
+                    answer[idx] = answer[idx] == -1 ? arr[i] : Math.min(answer[idx], arr[i]);
                 }
             }
+
         }
 
-        return result;
+        return answer;
+    }
+
+    public int[] solution2(int[] arr, int[][] queries) {
+        int[] answer = {};
+        return IntStream.range(0, queries.length)
+                .map(q -> IntStream.rangeClosed(queries[q][0], queries[q][1])
+                        .map(i -> arr[i])
+                        .filter(i -> i > queries[q][2])
+                        .min().orElse(-1)
+                ).toArray();
     }
 }
