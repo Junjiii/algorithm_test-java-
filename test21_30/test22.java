dd5d7cb3df4e6914d73d57056f5cc359706dbd53
Jjunjiii’ <drum1701@gmail.com>
2024-09-11 20:53:40 +0900

    test22

---
+++ test21_30/test22.java
@@ -0,0 +1,13 @@
+package test21_30;
+
+
+
+public class test22 {
+    public int[] solution(int start_num, int end_num) {
+        int[] arr = new int[end_num - start_num + 1];
+        for (int i = 0 ; i < arr.length; i++) {
+            arr[i] = start_num + i;
+        }
+        return arr;
+    }
+}
