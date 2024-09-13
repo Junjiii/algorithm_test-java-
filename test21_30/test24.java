a120e1c97480081c03c373bb9b741dd53a76e30e
Jjunjiii’ <drum1701@gmail.com>
2024-09-13 14:47:06 +0900

    test24

--- test21_30/test24.java
+++ test21_30/test24.java
@@ -29,5 +29,31 @@ public class test24 {
         }
         return Math.min(Math.min(a, b), Math.min(c, d));
     }
+
+    public int solution2(int a, int b, int c, int d) {
+
+        int[] dice = { a, b, c, d };
+        Arrays.sort(dice);
+
+        int ans = 0;
+
+        if (dice[0] == dice[3]) {
+            ans = 1111 * dice[3];
+        } else if (dice[0] == dice[2] || dice[1] == dice[3]) {
+            ans = (int) Math.pow(dice[1] * 10 + (dice[0] + dice[3] - dice[1]), 2);
+        } else if (dice[0] == dice[1] && dice[2] == dice[3]) {
+            ans = (dice[0] + dice[3]) * (dice[3] - dice[0]);
+        } else if (dice[0] == dice[1]) {
+            ans = dice[2] * dice[3];
+        } else if (dice[1] == dice[2]) {
+            ans = dice[0] * dice[3];
+        } else if (dice[2] == dice[3]) {
+            ans = dice[0] * dice[1];
+        } else {
+            ans = dice[0];
+        }
+
+        return ans;
+    }
 }
 
