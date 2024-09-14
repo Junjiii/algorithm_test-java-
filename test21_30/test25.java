1f6db480f503ce00bc969ee3e8559d66c58e86e6
Jjunjiii’ <drum1701@gmail.com>
2024-09-14 21:21:25 +0900

    test25

--- test21_30/test25.java
+++ test21_30/test25.java
@@ -15,4 +15,16 @@ public class test25 {
         }
         return result;
     }
+
+    public String solution2(String my_string, int[] index_list) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int idx : index_list)
+            sb.append(my_string.charAt(idx));
+
+        return sb.toString();
+    }
+
+
 }
