270ac68e07a1f4321b65d3210f7af97bbea858ac
Jjunjiii’ <drum1701@gmail.com>
2024-09-14 21:21:39 +0900

    test25

--- test21_30/test25.java
+++ test21_30/test25.java
@@ -1,5 +1,7 @@
 package test21_30;
 
+import java.util.Arrays;
+
 public class test25 {
     public static void main(String[] args) {
         int[] arr = {16, 6, 5, 3, 12, 14, 11, 11, 17, 12, 7};
@@ -24,6 +26,10 @@ public class test25 {
             sb.append(my_string.charAt(idx));
 
         return sb.toString();
+    }
+
+    public String solution(String myString, int[] indexList) {
+        return Arrays.stream(indexList).mapToObj(operand -> String.valueOf(myString.charAt(operand))).collect(Collectors.joining());
     }
 
 
