a3d1380da4676bde7a2b90f65562758f5173d5c7
Jjunjiii’ <drum1701@gmail.com>
2024-09-27 19:23:59 +0900

    test26

---
+++ test21_30/test26.java
@@ -0,0 +1,24 @@
+package test21_30;
+
+public class test26 {
+    char[] arr;
+
+    public String solution(String my_string, int[][] queries) {
+
+        arr = my_string.toCharArray();
+
+        for (int[] query : queries) {
+            reverse(query[0], query[1]);
+        }
+
+        return new String(arr);
+    }
+
+    private void reverse(int s, int e) {
+        while (s < e) {
+            char temp = arr[s];
+            arr[s++] = arr[e];
+            arr[e--] = temp;
+        }
+    }
+}
