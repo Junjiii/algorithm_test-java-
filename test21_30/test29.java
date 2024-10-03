00faf62db84cbb825f416fed3de9d1ae4868db8e
Jjunjiii’ <drum1701@gmail.com>
2024-10-03 17:07:46 +0900

    test29

--- test21_30/test29.java
+++ test21_30/test29.java
@@ -1,6 +1,7 @@
 package test21_30;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class test29 {
@@ -13,5 +14,16 @@ public class test29 {
 
     public String[] solution2(String myString) {
         return IntStream.range(0, myString.length()).mapToObj(myString::substring).sorted().toArray(String[]::new);
+    }
+
+    public String[] solution3(String my_string) {
+        String[] answer = new String[my_string.length()];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = my_string.substring(i, my_string.length());
+        }
+
+        Arrays.sort(answer);
+        return answer;
     }
 }
