55a61f336112725b5ef7b0c18a3949353daa44db
Jjunjiii’ <drum1701@gmail.com>
2024-10-05 13:19:56 +0900

    test30

--- test21_30/test30.java
+++ test21_30/test30.java
@@ -7,4 +7,16 @@ public class test30 {
     public int solution(String number) {
         return number.chars().map(c -> (c - '0') % 9).sum() % 9;
     }
+
+    public int solution2(String number) {
+        int answer = 0;
+
+        for(int i=0; i<number.length(); i++) {
+            answer += number.charAt(i) - '0';
+        }
+
+        answer %= 9;
+
+        return answer;
+    }
 }
