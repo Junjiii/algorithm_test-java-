f4de6d069b3d4fdc4593b7a7d2f74bfbe09b898a
Jjunjiii’ <drum1701@gmail.com>
2024-10-06 10:19:00 +0900

    test31

--- test31_40/test31.java
+++ test31_40/test31.java
@@ -8,4 +8,10 @@ public class test31 {
 
         return str1 + reverseStr + str2;
     }
+
+    public String solution2(String my_string, int s, int e) {
+        StringBuilder answer = new StringBuilder(my_string.substring(s, e + 1));
+        answer.reverse();
+        return my_string.substring(0, s) + answer + my_string.substring(e + 1);
+    }
 }
