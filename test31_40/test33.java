c4cf0bf0f939614b36ad081d25df3de58e88c420
Jjunjiii’ <drum1701@gmail.com>
2024-10-09 20:50:32 +0900

    test33

--- test31_40/test33.java
+++ test31_40/test33.java
@@ -1,8 +1,5 @@
 package test31_40;
 
-import java.util.Arrays;
-import java.util.stream.IntStream;
-
 public class test33 {
     public String solution(String my_string, int[] indices) {
         String answer = "";
